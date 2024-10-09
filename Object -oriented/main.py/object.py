b05f53ac2e295ec78a385098e22d9c11d53f562c
Ryan567dot <itsbluey1010@gmail.com>
2024-10-09 22:20:58 +0600

    object.py

--- Object -oriented/main.py/object.py
+++ Object -oriented/main.py/object.py
@@ -4,3 +4,19 @@ class Coffee:
     
 cap = Coffee()
 print(cap.milk)
+
+#Init
+
+class Car:
+    def _init_(self,name,model,price,seats,AC):
+        self.Name = name
+        self.Model = model
+        self.Price = price
+        self.Seats = seats
+        self.AC = AC
+        
+bmw = Car("BMW","V8","40000$",6,True)
+suzuki = Car("Suzuki","A2","200$",4,False)
+
+print(bmw.Price)
+print(suzuki.Model)
